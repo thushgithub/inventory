70188ef0bd4c7e9194f1017154b689b6555e25aa
Thushalan <isoftthushalan@gmail.com>
2020-07-25 16:49:56 +0530

    first commit

---
+++ InventoryManagement/Login.aspx.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Login : System.Web.UI.Page
+{
+    MySqlConnection sqlcon = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["mySQLconn"].ConnectionString);
+
+    //static SqlConnection sqlcon = new SqlConnection("Server=localhost;Port=3306;Database=pipds;User=root;Password;Option=3;");
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        sqlcon.Open();
+        string checkquery = "Select count(1) from Login where Username='"+txtUserName.Text+"' and Password='"+txtPassword.Text.Trim()+"'";
+        MySqlCommand cmd = new MySqlCommand(checkquery, sqlcon);
+        int count = Convert.ToInt32(cmd.ExecuteScalar());
+        if (count == 1)
+        {
+            //lblerror.Text = "login Successful!";
+
+            Session["user"] = txtUserName.Text.Trim();
+            Response.Redirect("Home.aspx");
+            
+        }
+        else
+        {
+            lblerror.Text = "Login Failed. Incorrect Username or Password!";
+        }
+        sqlcon.Close();
+    }
+
+    protected void btnRefresh_Click(object sender, EventArgs e)
+    {
+        txtUserName.Text = "";
+        txtPassword.Text = "";
+    }
+}
---
+++ InventoryManagement/Product.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+public partial class Product : System.Web.UI.Page
+{
+    MySqlConnection sqlcon = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["mySQLconn"].ConnectionString);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["user"] == null)
+        {
+            Response.Redirect("Login.aspx");
+        }
+        else if (!IsPostBack)
+        {
+            btndelete.Enabled = false;
+            FillGridView();
+        }
+    }
+
+    protected void btnclear_Click(object sender, EventArgs e)
+    {
+        this.clear();
+    }
+   
+    public void clear()
+    {
+        hfProductId.Value = "";
+        txtproname.Text = txtprodes.Text = "";
+        lblerrormessage.Text = lblsuccessmassage.Text = "";
+        btnsave.Text = "Save";
+        btndelete.Enabled = true;
+        
+    }
+
+    protected void btnsave_Click(object sender, EventArgs e)
+    {
+
+        if (sqlcon.State == ConnectionState.Closed)
+        sqlcon.Open();
+        MySqlCommand sqlcmd = new MySqlCommand("ProductCreateOrUpdate",sqlcon);
+        sqlcmd.CommandType = CommandType.StoredProcedure;
+        sqlcmd.Parameters.AddWithValue("@ProductId",hfProductId.Value==""?0:Convert.ToInt32(hfProductId.Value));
+        sqlcmd.Parameters.AddWithValue("@ProductName",txtproname.Text.Trim());
+        sqlcmd.Parameters.AddWithValue("@ProductDescription",txtprodes.Text.Trim());
+        sqlcmd.ExecuteNonQuery();
+        sqlcon.Close();
+        string ProductId = hfProductId.Value;
+        clear();
+
+        if (ProductId == "")
+            lblsuccessmassage.Text = "Saved Successfully";
+        else
+            lblsuccessmassage.Text = "Updated Successfully";
+        FillGridView();
+    }
+
+    void FillGridView()
+    {
+        if (sqlcon.State == ConnectionState.Closed) 
+            sqlcon.Open();
+        MySqlDataAdapter sqlDa = new MySqlDataAdapter("ProductViewAll",sqlcon);
+        sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+        DataTable dtbl = new DataTable();
+        sqlDa.Fill(dtbl);
+        sqlcon.Close();
+        productGrid.DataSource = dtbl;
+        productGrid.DataBind();
+    }
+
+    protected void lnk_onClick(object sender, EventArgs e)
+    {
+        int ProductId = Convert.ToInt32((sender as LinkButton).CommandArgument);
+        if (sqlcon.State == ConnectionState.Closed) 
+        sqlcon.Open();
+        MySqlDataAdapter sqlDa = new MySqlDataAdapter("ProductViewById", sqlcon);
+        sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+        sqlDa.SelectCommand.Parameters.AddWithValue("@ProductId", ProductId);
+        DataTable dtbl = new DataTable();
+        sqlDa.Fill(dtbl);
+        sqlcon.Close();
+        hfProductId.Value = ProductId.ToString();
+        txtproname.Text = dtbl.Rows[0]["ProductName"].ToString();
+        txtprodes.Text = dtbl.Rows[0]["ProductDescription"].ToString();
+        btnsave.Text = "Update";
+        btndelete.Enabled = true;
+        
+        
+    }
+
+    protected void btndelete_Click(object sender, EventArgs e)
+    {
+        if (sqlcon.State == ConnectionState.Closed)
+            sqlcon.Open();
+        MySqlCommand sqlcmd = new MySqlCommand("ProductDeleteById",sqlcon);
+        sqlcmd.CommandType = CommandType.StoredProcedure;
+        sqlcmd.Parameters.AddWithValue("@ProductId",Convert.ToInt32(hfProductId.Value));
+        sqlcmd.ExecuteNonQuery();
+        sqlcon.Close();
+        clear();
+        FillGridView();
+        lblsuccessmassage.Text = "Deleted Successfully";
+    }
+}
